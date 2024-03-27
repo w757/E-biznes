6789093a9242a7fd3ccb225e08465d0861c2487f
w757 <weronika.rzeszutek27@gmail.com>
2024-03-27 12:17:51 -0400

    frontend

--- Zadanie 5 - Frontend/client/src/App.js
+++ Zadanie 5 - Frontend/client/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Products from './components/Products';
+import Products from './components/ProductList';
 import Payments from './components/Payments';
 import Cart from './components/Cart';
+import axios from 'axios'; // Importuj axios
+import ProductList from './components/ProductList';
 
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/" exact component={Products} />
+          <Route path="/products" component={ProductList} />
           <Route path="/payments" component={Payments} />
           <Route path="/cart" component={Cart} />
         </Switch>
@@ -19,3 +21,6 @@ const App = () => {
 };
 
 export default App;
+
+
+
--- Zadanie 5 - Frontend/client/src/index.js
+++ Zadanie 5 - Frontend/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import axios from 'axios'; // Importuj axios 
 ReactDOM.render(
   <React.StrictMode>
     <App />
